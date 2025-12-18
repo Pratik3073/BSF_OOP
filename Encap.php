06f454c43294caad1c794852032a7a27dd3b9a4e
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-18 18:09:08 +0530

    Add Person class with age validation and constructor. Implemented private properties for name and age, and included methods for setting and getting age with validation logic. Added example usage for instantiation and age setting.

---
+++ Encap.php
@@ -0,0 +1,41 @@
+<?php
+
+class Person {
+
+ //public -Accessible from anywhere
+ //private - Accessible only inside the class
+    private $name;
+    private $age;
+
+    
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setAge($age)
+    {
+        if ($age < 18) {
+            
+            throw new Exception("Person is not old enough.");
+        }
+
+     
+        $this->age = $age;
+    }
+
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
+
+
+$pratik = new Person("Pratik Kadam");
+
+
+$pratik->setAge(25);
+
+var_dump($pratik->getAge());
+?>
