b42473845ae7d9857593ef45ac4d1440a735297f
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-18 17:16:19 +0530

    Enhance Task class with completion functionality and detailed comments. Added a completed property and a run method to mark tasks as complete. Improved constructor documentation for clarity.

--- Task.php
+++ Task.php
@@ -2,11 +2,30 @@
 class Task {
     public $description;
 
-    public function __construct($description){
-        $this->description = $description; // ✅ fixed
+    public $completed = false;
+    public function __construct($description)
+    {  
+        //Initializing data using a Constructor
+        //It sets initial values for the object
+        //$this refers to the current object
+         
+        $this->description = $description; 
+        
+    }
+
+    public function run()
+    {
+        $this->completed = true;
     }
 }
 
 $task = new Task("Learn OOP");
-var_dump($task->description);
+$task1 = new Task("Fix CMS bugs");
+
+//new Task() creates an object
+//"Learn OOP" is passed to the constructor
+//Stored inside $task->description
+
+$task->run();
+var_dump($task->completed);
 ?>
